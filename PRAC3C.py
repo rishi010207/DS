2cede32975f21c5c565d91fa5f5a3f5f05caa8a5
rishi010207 <60725629+rishi010207@users.noreply.github.com>
2020-10-29 12:25:39 +0530

    Add files via upload

---
+++ PRAC3C.py
@@ -0,0 +1,35 @@
+def add(A, B, m, n): 
+		size = max(m, n); 
+		sum = [0 for i in range(size)] 		
+		for i in range(0, m, 1): 
+			sum[i] = A[i] 	
+		for i in range(n): 
+			sum[i] += B[i] 
+		return sum
+
+def printPoly(poly, n): 
+		for i in range(n): 
+			print(poly[i], end = "") 
+			if (i != 0): 
+				print("x^", i, end = "") 
+			if (i != n - 1): 
+				print(" + ", end = "") 
+
+if __name__ == '__main__': 		
+		A = [6,1,11,7] 		
+		B = [2,3,5] 
+		m = len(A) 
+		n = len(B) 
+		print("First polynomial is") 
+		printPoly(A, m) 
+		print("\n", end = "") 
+		print("Second polynomial is") 
+		printPoly(B, n) 
+		print("\n", end = "") 
+		sum = add(A, B, m, n) 
+		size = max(m, n) 
+	
+		print("sum polynomial is") 
+		printPoly(sum, size) 
+		
+
